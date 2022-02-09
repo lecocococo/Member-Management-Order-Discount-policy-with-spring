b8af1c6c6e5a11e86af14bc6e95f04468ec3af04
lecocococo <chemwang13@naver.com>
2022-02-10 03:16:02 +0900

    Init

---
+++ src/test/java/hello/core/discount/RateDiscountPolicyTest.java
@@ -0,0 +1,41 @@
+package hello.core.discount;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class RateDiscountPolicyTest {
+
+    RateDiscountPolicy discountPolicy = new RateDiscountPolicy();
+
+    @Test
+    @DisplayName("VIP는 10%의 할인이 적용되야함")
+    void vip_yes(){
+        //give
+        Member member = new Member(1L, "memVIP", Grade.VIP);
+
+        //when
+        int discount = discountPolicy.discount(member, 10000);
+
+        //then
+        assertThat(discount).isEqualTo(1000);
+    }
+
+    @Test
+    @DisplayName("일반등급은 할인이 적용X")
+    void vip_no(){
+        //given
+        Member member = new Member(2L, "memBASIC", Grade.BASIC);
+
+        //when
+        int discount = discountPolicy.discount(member, 10000);
+
+        //then
+        assertThat(discount).isEqualTo(0);
+    }
+}
